96766355fdeecf5e365a547bd35400cf6876b453
jlewisit <joelewis.mortgage@gmail.com>
2022-09-03 09:37:42 -0700

    server.js draft complete

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const api = require('./Develop/public/assets/js/index.js');
+
+
+app.listen("3001", () =>
+console.log(`App is running now`)
+);
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use('/api', api);
+
+app.use(express.static('public'));
+
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, './Develop/public/index.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);

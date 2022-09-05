558328cb09d44c8b74da912508b743920beb5234
jlewisit <joelewis.mortgage@gmail.com>
2022-09-05 04:10:14 -0700

    .gitignore added

--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const api = require('./Develop/public/assets/js/index.js');
+const api = require('./public/assets/js/index.js');
 
 
 app.listen("3001", () =>
@@ -17,7 +17,7 @@ app.use(express.static('public'));
 
 // GET Route for homepage
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, './Develop/public/index.html'))
+  res.sendFile(path.join(__dirname, './public/index.html'))
 );
 
 app.listen(PORT, () =>

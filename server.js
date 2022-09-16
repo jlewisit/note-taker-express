bf69cbcb2f36eb0f76eecc04cc3e26837b0dd138
jlewisit <joelewis.mortgage@gmail.com>
2022-09-16 10:45:27 -0700

    update for bcs

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ const app = express();
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-// app.use('/api', api);
+// Syntax: app.use(path, callback)
+app.use('/api', api);
 
 app.use(express.static('public'));
 
